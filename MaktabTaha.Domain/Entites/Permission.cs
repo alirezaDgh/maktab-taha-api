d78f863329fa359811fd1f807b68348dafe8115d
clonerops <114849678+clonerops@users.noreply.github.com>
2026-09-24 11:11:46 -0700

    refactor user

--- MaktabTaha.Domain/Entites/Permission.cs
+++ MaktabTaha.Domain/Entites/Permission.cs
@@ -5,7 +5,15 @@ namespace MaktabTaha.Domain.Entites
     public class Permission : BaseEntity<int>
     {
         public string Title { get; set; }
+        public string Key { get; set; }
+        public string? Path { get; set; }
+        public string? Icon { get; set; }
+        public int? ParentId { get; set; }
+        public int SortOrder { get; set; }
+        public Permission? Parent { get; set; }
+        public ICollection<Permission> Children { get; set; } = new List<Permission>();
         public ICollection<UserPermission> UserPermissions { get; set; } = new List<UserPermission>();
+
 
     }
 }
